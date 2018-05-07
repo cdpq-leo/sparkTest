3a837f8f49e66f6da01987ce76ee8c8d06dda428
lpbonenfant <lpbonenfant@cdpq.com>
2018-05-07 15:23:02 -0400

    fix python script

--- job.py
+++ job.py
@@ -1,2 +1,16 @@
+conf = pyspark.SparkConf()
+conf.setMaster("mesos://leader.mesos:5050")
+conf.setAppName("Test Alluxio")
+conf.set("spark.mesos.executor.docker.image", "mesosphere/spark:2.1.0-2.2.0-1-hadoop-2.6")
+conf.set("spark.mesos.executor.home", "/opt/spark/dist")
+conf.set("spark.mesos.coarse", "true")
+conf.set("spark.driver.memory", "512m")
+conf.set("spark.executor.memory", "512m")
+conf.set("spark.executor.cores", "1")
+conf.set("spark.eventLog.enabled", "false")
+conf.set("spark.submit.deployMode", "client")
+
+sc = pyspark.SparkContext(conf=conf)
+
 file = sc.textFile("hdfs://hdfs/user/jsaba/data_etf.csv")
 file.count()
