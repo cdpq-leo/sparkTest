ab4ba5f8128fa209ec26afedba30925c604de33e
lpbonenfant <lpbonenfant@cdpq.com>
2018-05-09 11:49:03 -0400

    limited executors to 1, added more memory to executor

--- job.py
+++ job.py
@@ -7,11 +7,12 @@ conf.set("spark.mesos.executor.docker.image", "cdpqleo/spark:2.3.1-2.2.1-2-hadoo
 conf.set("spark.mesos.executor.home", "/opt/spark/dist")
 conf.set("spark.mesos.coarse", "true")
 conf.set("spark.driver.memory", "512m")
-conf.set("spark.executor.memory", "512m")
+conf.set("spark.executor.memory", "2g")
 conf.set("spark.executor.cores", "1")
 conf.set("spark.eventLog.enabled", "false")
 conf.set("spark.submit.deployMode", "client")
 conf.set("spark.executor.instances", "1")
+conf.set("spark.cores.max", "1")
 conf.set("spark.eventLog.enabled", "true")
 conf.set("spark.eventLog.dir", "hdfs://hdfs/app/spark/2.3.0/logs")
 
