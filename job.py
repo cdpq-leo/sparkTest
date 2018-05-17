09bf69d0cd452afded6d662cc9ddf01892c737a1
lpbonenfant-cdpq <38701419+lpbonenfant-cdpq@users.noreply.github.com>
2018-05-17 16:13:57 -0400

    Update job.py

--- job.py
+++ job.py
@@ -15,6 +15,7 @@ conf.set("spark.executor.instances", "1")
 conf.set("spark.cores.max", "1")
 conf.set("spark.eventLog.enabled", "true")
 conf.set("spark.eventLog.dir", "hdfs://hdfs/app/spark/2.3.0/logs")
+conf.set("spark.mesos.constraints", "privatenodetype:stateless")
 
 
 sc = pyspark.SparkContext(conf=conf)
