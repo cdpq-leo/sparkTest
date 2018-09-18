30aded5c6ef6b9028a4d3430f2cb9b4f4004f773
lpbonenfant-cdpq <38701419+lpbonenfant-cdpq@users.noreply.github.com>
2018-09-18 15:30:51 -0400

    Update jobAlluxio.py

--- jobAlluxio.py
+++ jobAlluxio.py
@@ -21,5 +21,5 @@ conf.set("spark.hadoop.fs.defaultFS", "alluxio://alluxio-master.alluxio.marathon
 sc = pyspark.SparkContext(conf=conf)
 sc._jsc.hadoopConfiguration().set('fs.alluxio.impl', 'alluxio.hadoop.FileSystem')
 file = sc.textFile("alluxio://alluxio-master.alluxio.marathon.mesos:19998:/user/jsaba/data_etf.csv")
-file.saveAsTextFile("alluxio://alluxio-master.alluxio.marathon.mesos:10102:/user/lpbonenfant/data.csv")
+file.saveAsTextFile("alluxio://alluxio-master.alluxio.marathon.mesos:19998:/user/lpbonenfant/data.csv")
 
